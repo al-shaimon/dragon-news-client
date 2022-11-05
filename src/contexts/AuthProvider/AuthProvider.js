bf3961e9f01257a78eac45f3b8095d8089997ebc
al-shaimon <alshaimon968@gmail.com>
2022-11-05 23:48:04 +0600

    m-61 Implement Login and Register

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,6 +1,13 @@
 import React, { createContext, useEffect, useState } from 'react';
 import app from '../../firebase/firebase.config';
-import { getAuth, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
 
 export const AuthContext = createContext();
 
@@ -11,6 +18,14 @@ const AuthProvider = ({ children }) => {
 
   const providerLogin = (provider) => {
     return signInWithPopup(auth, provider);
+  };
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
   };
 
   const logOut = () => {
@@ -28,7 +43,7 @@ const AuthProvider = ({ children }) => {
     };
   }, []);
 
-  const authInfo = { user, providerLogin, logOut };
+  const authInfo = { user, providerLogin, logOut, createUser, signIn };
 
   return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
 };
