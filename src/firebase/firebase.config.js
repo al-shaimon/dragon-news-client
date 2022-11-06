ab2f03b8fc46194503ee0598459e16f8be91ce46
al-shaimon <alshaimon968@gmail.com>
2022-11-06 22:25:28 +0600

    m-62 Environment variable for Firebase in Create React App

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCCKWCIG66xb2JJx_UJ4fTXXVL_DqgprjE',
-  authDomain: 'dragon-news-e2253.firebaseapp.com',
-  projectId: 'dragon-news-e2253',
-  storageBucket: 'dragon-news-e2253.appspot.com',
-  messagingSenderId: '24174147971',
-  appId: '1:24174147971:web:1eece9504019c1f4cddeb6',
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
